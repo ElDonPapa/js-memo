1b7ba9a1c7b30095f2d460fc79731e682c349994
ElDonPapa <26351793+ElDonPapa@users.noreply.github.com>
2024-03-31 00:00:21 +0100

    Add dynamic card generation

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 "use strict"
 
-const cards = document.querySelectorAll(".card");
+const cardContainer = document.querySelector(".card-container");
+let cards = document.querySelectorAll(".card");
 const btn = document.querySelector(".btn");
 let selectedCards = 0;
+
+setCardContainerWidth(7);
+
+// Generating cards
+for(let i = 0; i<10; i++) {
+    addNewCard("blue");
+}
 
 cards.forEach(card => {
     card.addEventListener("click", event => {
@@ -24,7 +32,7 @@ function selectCard(card) {
         card.getAttribute("selected") === "true" ? "false" : "true");
 
     if(card.getAttribute("selected") === "true") {
-        card.style.setProperty("--translateY", "-16px");
+        card.style.setProperty("--translateY", "-12px");
         selectedCards++;
     } else {
         card.style.setProperty("--translateY", "0px");
@@ -54,3 +62,19 @@ function setButtonState() {
         btn.classList.add("btn--disabled");
     }
 }
+
+function addNewCard(color = "") {
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+    if(color.trim().length !==0) newCard.classList.add("card--" + color);
+    cardContainer.appendChild(newCard);
+    cards = document.querySelectorAll(".card");
+}
+
+function setCardContainerWidth(n) {
+    // Take a number of cards as parameter (ie: 5 to have 5 cards per line);
+    const width = parseInt(window.getComputedStyle(cards[0]).width) * n + 16 * (n - 1);
+    console.log(width);
+    cardContainer.style.width = `${width}px`;
+}
+
