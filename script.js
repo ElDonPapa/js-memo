9fca4b3861b7b7f1aa3a1b1b7439a59fefd3ab15
ElDonPapa <26351793+ElDonPapa@users.noreply.github.com>
2024-03-30 20:33:19 +0100

    Put code in functions
    For readability sake

--- script.js
+++ script.js
@@ -5,30 +5,38 @@ const btn = document.querySelector(".btn");
 
 cards.forEach(card => {
     card.addEventListener("click", event => {
-        card.setAttribute("selected",
-            card.getAttribute("selected") === "true" ? "false" : "true");
-
-        if(card.getAttribute("selected") === "true") {
-            card.style.setProperty("--translateY", "-16px");
-        } else {
-            card.style.setProperty("--translateY", "0px");
-        }
+        selectCard(card);
     });
 })
 
 btn.addEventListener("click", event => {
     cards.forEach(card => {
-        if(card.getAttribute("selected") === "true") {
-            card.setAttribute("selected", "false");
-            card.setAttribute("flipped", card.getAttribute("flipped") === "true" ? "false" : "true");
-            card.style.setProperty("--translateY", "0");
-            if(card.getAttribute("flipped") === "true") {
-                card.style.setProperty("--rotateYFront", "0deg");
-                card.style.setProperty("--rotateYBack", "180deg");
-            } else {
-                card.style.setProperty("--rotateYFront", "180deg");
-                card.style.setProperty("--rotateYBack", "0deg");
-            }
-        }
+        flipCard(card);
     })
 });
+
+function selectCard(card) {
+    card.setAttribute("selected",
+        card.getAttribute("selected") === "true" ? "false" : "true");
+
+    if(card.getAttribute("selected") === "true") {
+        card.style.setProperty("--translateY", "-16px");
+    } else {
+        card.style.setProperty("--translateY", "0px");
+    }
+}
+
+function flipCard(card) {
+    if(card.getAttribute("selected") === "true") {
+        card.setAttribute("selected", "false");
+        card.setAttribute("flipped", card.getAttribute("flipped") === "true" ? "false" : "true");
+        card.style.setProperty("--translateY", "0");
+        if(card.getAttribute("flipped") === "true") {
+            card.style.setProperty("--rotateYFront", "0deg");
+            card.style.setProperty("--rotateYBack", "180deg");
+        } else {
+            card.style.setProperty("--rotateYFront", "180deg");
+            card.style.setProperty("--rotateYBack", "0deg");
+        }
+    }
+}
