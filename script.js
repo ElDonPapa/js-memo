d21291a5045acd476dc4b359e8ebd5aa5ba20547
ElDonPapa <26351793+ElDonPapa@users.noreply.github.com>
2024-03-30 20:26:28 +0100

    Add flip card functionality

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-"use script"
+"use strict"
 
 const cards = document.querySelectorAll(".card");
+const btn = document.querySelector(".btn");
 
 cards.forEach(card => {
     card.addEventListener("click", event => {
         card.setAttribute("selected",
             card.getAttribute("selected") === "true" ? "false" : "true");
 
-        const isSelected = card.getAttribute("selected") === "true";
-
-        if(isSelected) {
+        if(card.getAttribute("selected") === "true") {
             card.style.setProperty("--translateY", "-16px");
         } else {
             card.style.setProperty("--translateY", "0px");
         }
     });
 })
+
+btn.addEventListener("click", event => {
+    cards.forEach(card => {
+        if(card.getAttribute("selected") === "true") {
+            card.setAttribute("selected", "false");
+            card.setAttribute("flipped", card.getAttribute("flipped") === "true" ? "false" : "true");
+            card.style.setProperty("--translateY", "0");
+            if(card.getAttribute("flipped") === "true") {
+                card.style.setProperty("--rotateYFront", "0deg");
+                card.style.setProperty("--rotateYBack", "180deg");
+            } else {
+                card.style.setProperty("--rotateYFront", "180deg");
+                card.style.setProperty("--rotateYBack", "0deg");
+            }
+        }
+    })
+});
